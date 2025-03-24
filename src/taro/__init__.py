cc5715a8511c11a8c386647db038d4ee93f072ac
jonas-witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2025-03-24 13:45:06 +0100

    fix get version

--- src/taro/__init__.py
+++ src/taro/__init__.py
@@ -1,7 +1,7 @@
 
 from importlib.metadata import version, PackageNotFoundError
 try:
-    __version__ = version("taro")
+    __version__ = version("limmaco-rad")
 except PackageNotFoundError:
     # package is not installed
     pass
